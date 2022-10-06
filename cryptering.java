c5c8c62baf662e988de37e43c3c0479746e08e47
nosskire1 <viktor.eriksson3@elev.ga.ntig.se>
2022-10-06 13:42:48 +0200

    .

--- cryptering.java
+++ cryptering.java
@@ -1,2 +1,13 @@
 public class cryptering {
+    public static void main(String[] args){
+        String m = "hej";
+        String k = "hallo";
+
+        Model cryptModel = new Model();
+        System.out.print(cryptModel.cryptString(m,k));
+    }
+
+
+
+
 }
